0de5f7f473c87ebfbd6f5fbf1ee727fd94b17dde
Donatas <petrdonatas@gmail.com>
2022-09-23 19:16:07 +0300

    Add saving group

--- features/adminFinance/constants/savingGroups.ts
+++ features/adminFinance/constants/savingGroups.ts
@@ -4,3 +4,4 @@ export const SAVING_GROUP_BUSINESS_DZERALDA = 'business_dzeralda';
 export const SAVING_GROUP_BUSINESS_DONATAS = 'business_donatas';
 export const SAVING_GROUP_REAL_ESTATE = 'real_estate';
 export const SAVING_GROUP_INCOME = 'income';
+export const SAVING_GROUP_FOOD_BUITIS = 'maistas/buitis';
